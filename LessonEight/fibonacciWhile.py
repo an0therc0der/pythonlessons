a94673b1b4b35446bbfa985f6f8802d7a348db25
an0therc0der <42703132+an0therc0der@users.noreply.github.com>
2018-08-30 21:25:26 -0400

    Update fibonacciWhile.py

--- LessonEight/fibonacciWhile.py
+++ LessonEight/fibonacciWhile.py
@@ -1,3 +1,8 @@
+"""
+Write a program that takes a number from a user and prints all fibonacci numbers
+that are less than that number to the screen. Assume that the user provides proper
+input for this exercise.
+"""
 previousFib = 1
 currentFib = 1
 
@@ -9,7 +14,9 @@ else:
   print (currentFib)
   while currentFib < userLimit:
     print (currentFib)
+    #Calculate the Next Number
     nextFib = previousFib + currentFib
+    #Update variables to setup the next iteration
     previousFib = currentFib
     currentFib = nextFib
 
